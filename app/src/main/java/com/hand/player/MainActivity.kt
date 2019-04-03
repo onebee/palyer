e086ec070103aabf63f3a8039b8bb5df21aa71a3
Kaibin <diaokaibin@gmail.com>
2019-04-03 23:20:40 +0800

    Initial commit

---
+++ app/src/main/java/com/hand/player/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hand.player
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
