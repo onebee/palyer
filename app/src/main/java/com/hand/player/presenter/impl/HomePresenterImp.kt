6700b7271f3aac5b8fedf057aeafe56babe8df51
Kaibin <diaokaibin@gmail.com>
2019-04-06 12:21:03 +0800

    update

--- app/src/main/java/com/hand/player/presenter/impl/HomePresenterImp.kt
+++ app/src/main/java/com/hand/player/presenter/impl/HomePresenterImp.kt
@@ -18,93 +18,55 @@ import java.io.IOException
  */
 class HomePresenterImp(var homeView: HomeView) : HomePresenter {
     override fun loadData() {
-
         val path = URLProviderUtils.getHomeUrl(0, 20)
-
         val client = OkHttpClient.Builder().build()//addInterceptor(StethoInterceptor()).build()
         val request = Request.Builder().url(path).get().build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-//                info { " 当前线程 : " + Thread.currentThread().name }
-//                myToash("刷新数据失败 ! " + e.printStackTrace())
-
                 ThreadUtil.runOnMainThread(object : Runnable {
                     override fun run() {
                         // 回到view 层处理
                         homeView.onError(e?.message)
-
-//                        sw_layout.isRefreshing = false
                     }
-
                 })
             }
 
             override fun onResponse(call: Call, response: Response) {
-//                info { " 当前线程 : " + Thread.currentThread().name }
-
-//                myToash("刷新数据成功 ! ")
                 val result = response.body()?.string()
                 val gson = Gson()
                 val list = gson.fromJson<List<HomeItemBean>>(result, object : TypeToken<List<HomeItemBean>>() {}.type)
                 ThreadUtil.runOnMainThread(object : Runnable {
                     override fun run() {
-//                        adapter.update(list)
-//                        sw_layout.isRefreshing = false
                         homeView.loadSuccess(list)
                     }
-
                 })
-
             }
-
-
         })
-
-
-
     }
 
     override fun loadMoreData(offset: Int) {
-
         val path = URLProviderUtils.getHomeUrl(offset, 20)
-
         val client = OkHttpClient.Builder().build()//addInterceptor(StethoInterceptor()).build()
         val request = Request.Builder().url(path).get().build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-//                info { " 当前线程 : " + Thread.currentThread().name }
-//                myToash("刷新数据失败 ! " + e.printStackTrace())
-
                 ThreadUtil.runOnMainThread(object : Runnable {
                     override fun run() {
-//                        sw_layout.isRefreshing = false
                         homeView.onError(e?.message)
-
                     }
-
                 })
             }
 
             override fun onResponse(call: Call, response: Response) {
-//                info { " 当前线程 : " + Thread.currentThread().name }
-
-//                myToash("刷新数据成功 ! ")
                 val result = response.body()?.string()
                 val gson = Gson()
                 val list = gson.fromJson<List<HomeItemBean>>(result, object : TypeToken<List<HomeItemBean>>() {}.type)
                 ThreadUtil.runOnMainThread(object : Runnable {
                     override fun run() {
-//                        adapter.loadMore(list)
-//                        sw_layout.isRefreshing = false
                         homeView.loadMore(list)
-
                     }
-
                 })
-
             }
-
-
         })
     }
 
