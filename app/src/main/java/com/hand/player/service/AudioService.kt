2d7e46eaea3364b0a0cd9025cd4117bc51c7ae0d
Kaibin <diaokaibin@gmail.com>
2019-04-11 22:39:00 +0800

    服务绑定 播放歌曲

---
+++ app/src/main/java/com/hand/player/service/AudioService.kt
@@ -0,0 +1,65 @@
+package com.hand.player.service
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+import com.hand.player.model.AudioBean
+
+/**
+ * @author  diaokaibin@gmail.com on 2019/4/11.
+ */
+class AudioService : Service() {
+
+    var list: ArrayList<AudioBean>? = null
+    var position: Int = 0
+    //    val mediaPlayer by lazy { MediaPlayer() }
+    var mediaPlayer: MediaPlayer? = null
+
+    val binder by lazy { AudioBinder() }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onStart(intent: Intent?, startId: Int) {
+        super.onStart(intent, startId)
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        list = intent?.getParcelableArrayListExtra<AudioBean>("list")
+        position = intent?.getIntExtra("position", -1) ?: -1
+
+        binder.playItem()
+
+        return super.onStartCommand(intent, flags, startId)
+
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+
+        return binder
+
+    }
+
+    inner class AudioBinder : Binder(), MediaPlayer.OnPreparedListener {
+        override fun onPrepared(mp: MediaPlayer?) {
+            mediaPlayer?.start()
+
+        }
+
+        fun playItem() {
+            mediaPlayer = MediaPlayer()
+
+            mediaPlayer?.let {
+                it.setOnPreparedListener(this)
+                it.setDataSource(list?.get(position)?.data)
+                it.prepareAsync()
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/hand/player/ui/activity/AudioPlayerActivity.kt
+++ app/src/main/java/com/hand/player/ui/activity/AudioPlayerActivity.kt
@@ -1,9 +1,14 @@
 package com.hand.player.ui.activity
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
 import com.hand.player.R
 import com.hand.player.base.BaseActivity
 import com.hand.player.model.AudioBean
-import org.jetbrains.anko.info
+import com.hand.player.service.AudioService
 
 /**
  * @author  diaokaibin@gmail.com on 2019/4/11.
@@ -13,12 +18,46 @@ class AudioPlayerActivity : BaseActivity() {
         return R.layout.activity_audio_player
     }
 
+    val connection by lazy { AudioConnection() }
+
     override fun initData() {
 
         val list = intent.getParcelableArrayListExtra<AudioBean>("list")
         val position = intent.getIntExtra("position",-1)
 
-        info { list.toString() }
-        info { position.toString() }
+//        info { list.toString() }
+//        info { position.toString() }
+
+//        val mediaPlayer =  MediaPlayer()
+//        mediaPlayer.setOnPreparedListener{
+//
+//            mediaPlayer.start()
+//        }
+//
+//        mediaPlayer.setDataSource(list.get(position).data)
+//        mediaPlayer.prepareAsync()
+
+
+        val intent = Intent(this, AudioService::class.java)
+        intent.putExtra("list",list)
+        intent.putExtra("position",position)
+        startService(intent)
+
+        bindService(intent,connection,Context.BIND_AUTO_CREATE)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(connection)
+    }
+
+    class AudioConnection : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        }
+
     }
 }
