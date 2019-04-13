106338ba1ff0a1cd7e05cd1647d7f75b624f50cb
Kaibin <diaokaibin@gmail.com>
2019-04-13 21:32:49 +0800

    LyricView 绘制多行歌词

---
+++ app/src/main/java/com/hand/player/model/LrcBean.kt
@@ -0,0 +1,7 @@
+package com.hand.player.model
+
+/**
+ * @author  diaokaibin@gmail.com on 2019/4/13.
+ */
+data class LrcBean(var startTime:Int,var content:String) {
+}
--- app/src/main/java/com/hand/player/widget/LyricView.kt
+++ app/src/main/java/com/hand/player/widget/LyricView.kt
@@ -7,14 +7,19 @@ import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import com.hand.player.R
+import com.hand.player.model.LrcBean
 
 /**
  * @author  diaokaibin@gmail.com on 2019/4/13.
  */
 class LyricView : View {
 
-    val panint by lazy { Paint(Paint.ANTI_ALIAS_FLAG) }
+    val paint by lazy { Paint(Paint.ANTI_ALIAS_FLAG) }
 
+
+    val list by lazy { ArrayList<LrcBean>() }
+
+    var centerLine = 10
 
     var viewW: Int = 0
     var viewH: Int = 0
@@ -23,6 +28,7 @@ class LyricView : View {
     var smallSize = 0f
     var white = 0
     var green = 0
+    var lineHeight = 0
 
 
     constructor(context: Context?) : super(context)
@@ -34,30 +40,81 @@ class LyricView : View {
         smallSize = resources.getDimension(R.dimen.smallSize)
         white = resources.getColor(R.color.white)
         green = resources.getColor(R.color.green)
+
+        lineHeight = resources.getDimensionPixelOffset(R.dimen.lineHeight)
+        //
+        paint.textAlign = Paint.Align.CENTER
+
+        //循环添加歌词
+        for (i in 0 until 29) {
+            list.add(LrcBean(2000 * i, "在在播放第$i 行歌词"))
+        }
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        panint.textSize = bigSize
-        panint.color = green
+//        drawSingleLine(canvas)
+        drawMultiLine(canvas)
+    }
+
+    /***
+     * 绘制多行居中
+     */
+    private fun drawMultiLine(canvas: Canvas?) {
+        val centerText = list[centerLine].content
+        val bounds = Rect()
+
+        paint.getTextBounds(centerText, 0, centerText.length, bounds)
+
+        val textH = bounds.height()
+
+        // 居中行y
+        val centerY = viewH / 2 + textH / 2
+
+        for ((index, value) in list.withIndex()) {
+            if (index == centerLine) {
+
+                paint.color = green
+                paint.textSize = bigSize
+
+            } else {
+                paint.color = white
+                paint.textSize = smallSize
+            }
+
+            val curX = viewW / 2
+            val curY = centerY + (index - centerLine) * lineHeight
+            val curText = list[index].content
+            canvas?.drawText(curText, curX.toFloat(), curY.toFloat(), paint)
+
+        }
+
+
+    }
+
+    /**
+     * 绘制单行居中
+     */
+    private fun drawSingleLine(canvas: Canvas?) {
+        paint.textSize = bigSize
+        paint.color = green
 
 
         val text = "loadingLLLLLLLLLL"
 
         val bounds = Rect()
-        panint.getTextBounds(text, 0, text.length, bounds)
+        paint.getTextBounds(text, 0, text.length, bounds)
 
 
         val textW = bounds.width()
         val textH = bounds.height()
 
-        val x = viewW / 2 - textW / 2
+        //        val x = viewW / 2 - textW / 2
         val y = viewH / 2 + textH / 2
 
-        canvas?.drawText(text, x.toFloat(), y.toFloat(), panint)
-
-
+        canvas?.drawText(text, viewW / 2f, y.toFloat(), paint)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
